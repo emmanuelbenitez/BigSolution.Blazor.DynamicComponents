ef332f811b3db270386c2923a130275313351d32
Emmanuel Benitez <emmanuel.benitez@gmail.com>
2021-01-27 23:04:29 +0100

    Update framework to .Net5.0, submodules,YAML & nuget packages
    Include dev branches in CI

--- src/Blazor.DynamicComponents.Tests/DynamicElementFixture.cs
+++ src/Blazor.DynamicComponents.Tests/DynamicElementFixture.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-// Copyright © 2020 - 2020 Emmanuel Benitez
+// Copyright © 2020 - 2021 Emmanuel Benitez
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using Bunit;
-using Bunit.Rendering;
 using FluentAssertions;
 using Xunit;
 
